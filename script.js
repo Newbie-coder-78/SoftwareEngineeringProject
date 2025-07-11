f8c143e1cacacf4ae8f776de25838c1e8d8f2463
Newbie-coder-78 <pkarmungi1@gmail.com>
2025-07-10 20:55:18 -0400

    Update script.js to include a livecheck

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-//Theme toggle logic
+//DOM Elements
 const toggleBtn = document.getElementById('theme-toggle');
 const html = document.documentElement;
+const form = document.getElementById('password-form');
+const lengthInput = document.getElementById('length');
+const passwordField = document.getElementById('generated-password');
+const generateBtn = document.getElementById('generate-btn');
+const warning = document.getElementById('warning-msg');
+const strengthBar = document.getElementById('strength-btn');
+const copyBtn = document.getElementById('copy-btn');
 
+//Disable generate if length < 8 (live check)
+lengthInput.addEventListener('input', () => {
+    const value = parseInt(lengthInput.value);
+
+    if (value < 8 || isNaN(value)) {
+        generateBtn.disabled = true;
+        generateBtn.classList.add('disabled');
+        warning.textContent = "⚠️ Password must be at least 8 characters."
+    } else {
+        generateBtn.disabled = false;
+        generateBtn.classList.remove('disabled');
+        warning.textContent = "";
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const value = parseInt(lengthInput.value);
+    if (value < 8 || isNaN(value)) {
+        generateBtn.disabled = true;
+        generateBtn.classList.add('disabled');
+        warning.textContent = "⚠️ Password must be at least 8 characters.";
+    }
+});
+
+//Theme toggle
 function updateToggleText(theme) {
     const oppositeTheme = theme === 'light' ? 'Dark' : 'Light';
     toggleBtn.textContent = `Switch to ${oppositeTheme} Mode`;
